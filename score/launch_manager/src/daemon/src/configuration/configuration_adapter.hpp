815564f9917d90202086bd71cb0a35d50197ed9f
William Roebuck <244554584+WilliamRoebuck@users.noreply.github.com>
2026-08-10 13:09:22 +0100

    Move process_state.hpp

--- score/launch_manager/src/daemon/src/configuration/configuration_adapter.hpp
+++ score/launch_manager/src/daemon/src/configuration/configuration_adapter.hpp
@@ -17,7 +17,7 @@
 #include "score/mw/launch_manager/common/constants.hpp"
 #include "score/mw/launch_manager/common/identifier_hash.hpp"
 #include "score/mw/launch_manager/common/process_group_state_id.hpp"
-#include "score/mw/launch_manager/common/process_state.hpp"
+#include "score/mw/launch_manager/process_group_manager/process_state.hpp"
 #include "score/mw/launch_manager/configuration/config.hpp"
 #include "score/mw/launch_manager/process_group_manager/iprocess.hpp"
 #include <chrono>
