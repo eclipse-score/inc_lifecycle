f412534535117aa9db40c5197f2b791624d19a27
William Roebuck <244554584+WilliamRoebuck@users.noreply.github.com>
2026-08-31 10:51:16 +0100

    Apply suggestions

--- score/launch_manager/src/daemon/src/process_group_manager/details/process_info_node.cpp
+++ score/launch_manager/src/daemon/src/process_group_manager/details/process_info_node.cpp
@@ -43,9 +43,10 @@ ProcessInfoNode::ProcessInfoNode(
 
     const configuration::ApplicationProfile& app_profile = config_.component_properties.application_profile;
 
-    if (app_profile.alive_supervision.has_value() &&
-        app_profile.application_type == configuration::ApplicationType::ReportingAndSupervised)
+    if (app_profile.application_type == configuration::ApplicationType::ReportingAndSupervised)
     {
+        SCORE_LANGUAGE_FUTURECPP_ASSERT_DBG_MESSAGE(
+            app_profile.alive_supervision.has_value(), "Supervised process did not have alive supervision config");
         const IdentifierHash name{config_.name};
         const uid_t uid = config_.deployment_config.sandbox.uid;
 
